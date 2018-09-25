eafcbb7b5c738dcfde7c361fe6b0d038cf9085c8
babbincocks <zabbybab@gmail.com>
2018-09-24 21:50:57 -0400

    Initial upload

---
+++ XMLtoSQL/XMLtoSQL/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace XMLtoSQL
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgData = new System.Windows.Forms.DataGridView();
+            this.ofdFile = new System.Windows.Forms.OpenFileDialog();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgData
+            // 
+            this.dgData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgData.Location = new System.Drawing.Point(12, 24);
+            this.dgData.Name = "dgData";
+            this.dgData.Size = new System.Drawing.Size(465, 244);
+            this.dgData.TabIndex = 0;
+            this.dgData.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgData_RowHeaderMouseClick);
+            this.dgData.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgData_RowHeaderMouseClick);
+            // 
+            // ofdFile
+            // 
+            this.ofdFile.Filter = "XML Files|*.xml";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(496, 24);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(92, 37);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load XML File";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(496, 231);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(92, 37);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save to Database";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 299);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dgData);
+            this.Name = "frmMain";
+            this.Text = "XML to SQL";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgData;
+        private System.Windows.Forms.OpenFileDialog ofdFile;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ XMLtoSQL/XMLtoSQL/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace XMLtoSQL
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        DataSet xmlData = new DataSet();
+        int chosenID = 0;
+        //BindingSource source = new BindingSource();
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (ofdFile.ShowDialog() == DialogResult.OK)
+            {
+                
+                xmlData.ReadXml(ofdFile.FileName);
+                xmlData.Tables[0].Columns.Add("ID");
+                dgData.DataSource = xmlData.Tables[0];
+
+                
+                foreach (DataGridViewRow dgRow in dgData.Rows)
+                {
+                    int lastCell = dgRow.Cells.Count - 1;
+
+                    dgRow.Cells[lastCell].Value = dgRow.Index + 1;
+                }
+
+
+
+            }
+        }
+
+        //This also fires if a cell is clicked.
+        private void dgData_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            chosenID = e.RowIndex + 1;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlConn = new SqlConnection(@"Server=PL1\MTCDB;Database=Sandbox;Trusted_Connection=True;");
+
+            SqlCommand newComm = new SqlCommand("sp_InsertXML", sqlConn);
+
+            if(dgData.Rows.Count > 0)
+            {
+
+
+            }
+
+
+        }
+
+    }
+}
